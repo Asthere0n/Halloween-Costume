ed8129515e3a5b1b708e4ef1528f014bbc0790b6
Asthere0n <cristiansanchezsonido@gmail.com>
2024-10-11 23:23:44 +0100

    Frist commit

--- index.js
+++ index.js
@@ -1,43 +1,9 @@
+// Import the Cloudinary class.
+import {Cloudinary} from "@cloudinary/url-gen";
 
-import { v2 as cloudinary } from 'cloudinary';
-
-(async function() {
-
-    // Configuration
-    cloudinary.config({ 
-        cloud_name: 'dq2ssgtxk', 
-        api_key: '146965199693525', 
-        api_secret: 'jbQva1Wk6pYq-eJVfk0g4V_Cvp4' // Click 'View API Keys' above to copy your API secret
-    });
-    
-    // Upload an image
-     const uploadResult = await cloudinary.uploader
-       .upload(
-           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
-               public_id: 'shoes',
-           }
-       )
-       .catch((error) => {
-           console.log(error);
-       });
-    
-    console.log(uploadResult);
-    
-    // Optimize delivery by resizing and applying auto-format and auto-quality
-    const optimizeUrl = cloudinary.url('shoes', {
-        fetch_format: 'auto',
-        quality: 'auto'
-    });
-    
-    console.log(optimizeUrl);
-    
-    // Transform the image: auto-crop to square aspect_ratio
-    const autoCropUrl = cloudinary.url('shoes', {
-        crop: 'auto',
-        gravity: 'auto',
-        width: 500,
-        height: 500,
-    });
-    
-    console.log(autoCropUrl);    
-})();
+// Create a Cloudinary instance and set your cloud name.
+const cld = new Cloudinary({
+  cloud: {
+    cloudName: 'demo'
+  }
+});
